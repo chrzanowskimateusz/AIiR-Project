a967a53356e7fa2e3ebbb9c07cc5a3e05dbfdf7b
Mateusz Chrzanowski <59821589+chrzanowskimateusz@users.noreply.github.com>
2020-06-09 16:30:27 +0200

    Styling fixes

--- Backend/code/traveling_salesman/users/forms.py
+++ Backend/code/traveling_salesman/users/forms.py
@@ -5,12 +5,13 @@ from django.contrib.auth import authenticate, get_user_model
 User = get_user_model()
 
 
+
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Add a valid email address.')
 
     class Meta:
         model = User
-        fields = ('email', 'username',)
+        fields = ('email', 'username')
 
 
 class AccountAuthenticationForm(forms.ModelForm):
